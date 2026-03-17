487c463a24c4d8a1d20abf54c7d7c6805586ef70
gabrielezed <gabriele.pala00@outlook.it>
2026-03-17 11:07:16 +0100

    Added dynamic jagged array

--- dataStructures/test.c
+++ dataStructures/test.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
-#include "genDynArr.h"
+#include "genJaggedArr.h"
 
-DEFINE_DYNARR(double, DoubleArr)
+DEFINE_JAGGEDARR(int, IntRow, IntMatrix)
 
 int main(void){
-	DoubleArr test;
-	DoubleArr_init(&test);
-	DoubleArr_push(&test, 440);
-	printf("Stored value is: %f", test.data[0]);
-	DoubleArr_free(&test);
+	printf("Hello, Sassari\n");
+	
+	IntMatrix myMatrix;
+	IntMatrix_init(&myMatrix);
+	
+	IntMatrix_push_at(&myMatrix, 0, 10);
+	IntMatrix_push_at(&myMatrix, 0, 20);
+	IntMatrix_push_at(&myMatrix, 0, 33);
+	
+	IntMatrix_push_at(&myMatrix, 2, 876);
+	
+	for(size_t i = 0; i < myMatrix.len; i++){
+		printf("Riga %zu (lunghezza %zu): ", i, myMatrix.data[i].len);
+		for(size_t j = 0; j < myMatrix.data[i].len; j++){
+			printf("%d ", myMatrix.data[i].data[j]);
+		}
+		printf("\n");
+	}
+	
+	IntMatrix_deep_free(&myMatrix);
 	
 	return 0;
 }
